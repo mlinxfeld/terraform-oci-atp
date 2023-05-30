32d2dffb815715cc0cd20249d180118e4a6be07f
mlinxfeld <martin.linxfeld@foggykitchen.com>
2023-05-30 10:00:36 +0000

    Updated provider and variables

--- provider.tf
+++ provider.tf
@@ -11,26 +11,26 @@ provider "oci" {
   alias            = "primaryregion"
   tenancy_ocid     = var.tenancy_ocid
   region           = local.primaryregion
-#  fingerprint      = var.fingerprint
-#  user_ocid        = var.user_ocid
-#  private_key_path = var.private_key_path
+  fingerprint      = var.fingerprint
+  user_ocid        = var.user_ocid
+  private_key_path = var.private_key_path
 }
 
 provider "oci" {
   alias            = "standbyregion"
   tenancy_ocid     = var.tenancy_ocid
   region           = local.standbyregion
-#  fingerprint      = var.fingerprint
-#  user_ocid        = var.user_ocid
- # private_key_path = var.private_key_path
+  fingerprint      = var.fingerprint
+  user_ocid        = var.user_ocid
+  private_key_path = var.private_key_path
 }
 
 provider "oci" {
   alias                = "homeregion"
   tenancy_ocid         = var.tenancy_ocid
-#  user_ocid            = var.user_ocid
-#  fingerprint          = var.fingerprint
-#  private_key_path     = var.private_key_path
+  user_ocid            = var.user_ocid
+  fingerprint          = var.fingerprint
+  private_key_path     = var.private_key_path
   region               = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
   disable_auto_retries = "true"
 }
